1000928ff25bc3ed8bca0cbc195c61f8456bb410
kihno <stistyle13@gmail.com>
2022-04-12 13:23:18 -0700

    Create module pattern to render homepage

--- src/index.js
+++ src/index.js
@@ -1,11 +1,41 @@
 import './style.css';
 
-function home() {
-    const element = document.createElement('div');
+const Home = (() => {
 
-    element.textContent = 'does this work?';
+    const content = document.getElementById('content');
 
-    return element;
-}
+    function renderNavbar() {
+        const navbar = document.createElement('ul');
 
-document.body.appendChild(home());
+        const links = ['Home', 'Menu', 'Contact'];
+
+        navbar.setAttribute('id', 'navbar');
+        for (let i = 0; i < links.length; i++) {
+            const li = document.createElement('li');
+
+            li.textContent = links[i];
+            navbar.appendChild(li);
+        }
+
+        content.appendChild(navbar);
+    }
+
+    function renderLogo() {
+        const h1 = document.createElement('h1');
+
+        h1.textContent = 'Brekkie';
+        content.appendChild(h1);
+    }
+
+    function renderTagline() {
+        const p = document.createElement('p');
+
+        p.textContent = "It's never too late to start your day with a little Brekkie.";
+        content.appendChild(p);    
+    }
+
+    renderNavbar();
+    renderLogo();
+    renderTagline();
+})();
+
