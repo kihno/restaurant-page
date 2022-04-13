34f0593b4ecdf01616a9d6b5bd025dacf9008877
kihno <stistyle13@gmail.com>
2022-04-13 13:12:26 -0700

    Initialize menu and contact page modules

---
+++ src/contact.js
@@ -0,0 +1,37 @@
+import {content, renderNavbar } from './index.js';
+
+function renderContact() {
+    const container = document.getElementById('container');
+
+    content.remove();
+    // const form = document.createElement('form');
+    // form.setAttribute('id', 'form');
+
+    // const formArray = ['name', 'email', 'message'];
+
+    // formArray.forEach(item => {
+    //     const label = document.createElement('label');
+    //     const input = document.createElement('input');
+    //     const textarea = document.createElement('textarea');
+
+    //     if (item === 'message') {
+    //         label.textContent = item
+    //         label.setAttribute('for', item)
+    //         textarea.setAttribute('id', item)
+
+    //         label.appendChild(textarea)
+    //         form.appendChild(label)
+    //     } else {
+    //         label.textContent = item
+    //         label.setAttribute('for', item)
+    //         input.setAttribute('id', item)
+
+    //         label.appendChild(input)
+    //         form.appendChild(label)
+    //     }
+    // });
+
+    // content.appendChild(form);
+}
+
+export { renderContact };
--- src/index.js
+++ src/index.js
@@ -1,78 +1,45 @@
 import './style.css';
+import { renderContact } from './contact.js'
 
-const home = (() => {
+const content = document.getElementById('content');
 
-    const content = document.getElementById('content');
+function renderNavbar() {
+    const navbar = document.createElement('ul');
 
-    function renderNavbar() {
-        const navbar = document.createElement('ul');
+    const links = ['home', 'menu', 'contact'];
 
-        const links = ['home', 'menu', 'contact'];
+    navbar.setAttribute('id', 'navbar');
+    for (let i = 0; i < links.length; i++) {
+        const li = document.createElement('li');
 
-        navbar.setAttribute('id', 'navbar');
-        for (let i = 0; i < links.length; i++) {
-            const li = document.createElement('li');
-
-            li.textContent = links[i];
-            li.setAttribute('id', `${links[i]}`)
-            navbar.appendChild(li);
-        }
-
-        content.appendChild(navbar);
+        li.textContent = links[i];
+        li.setAttribute('id', `${links[i]}`)
+        navbar.appendChild(li);
     }
 
-    function renderLogo() {
-        const h1 = document.createElement('h1');
-        const p = document.createElement('p');
-        const container = document.createElement('div');
+    content.appendChild(navbar);
+}
 
-        container.setAttribute('id', "logo");
+function renderLogo() {
+    const h1 = document.createElement('h1');
+    const p = document.createElement('p');
+    const container = document.createElement('div');
 
-        h1.textContent = 'Brekkie';
-        container.appendChild(h1);
+    container.setAttribute('id', "logo");
 
-        p.textContent = "It's never too late to start your day with a little Brekkie.";
-        container.appendChild(p); 
+    h1.textContent = 'Brekkie';
+    container.appendChild(h1);
 
-        content.appendChild(container);
-    }
+    p.textContent = "It's never too late to start your day with a little Brekkie.";
+    container.appendChild(p); 
 
-    renderNavbar();
-    renderLogo();
+    content.appendChild(container);
+}
 
-})();
+renderNavbar();
+renderLogo();
 
-const contact = (() => {
-    const content = document.getElementById('content');
-    const form = document.createElement('form');
-    form.setAttribute('id', 'form');
+const contact = document.getElementById('contact');
+contact.onclick = renderContact();
 
-    const formArray = ['name', 'email', 'message'];
-
-    formArray.forEach(item => {
-        const label = document.createElement('label');
-        const input = document.createElement('input');
-        const textarea = document.createElement('textarea');
-
-        if (item === 'message') {
-            label.textContent = item
-            label.setAttribute('for', item)
-            textarea.setAttribute('id', item)
-
-            label.appendChild(textarea)
-            form.appendChild(label)
-        } else {
-            label.textContent = item
-            label.setAttribute('for', item)
-            input.setAttribute('id', item)
-
-            label.appendChild(input)
-            form.appendChild(label)
-        }
-    });
-    
-    const container = document.getElementById('container');
-    content.remove(container);
-    content.appendChild(form);
-});
-
+export { content, renderNavbar }
